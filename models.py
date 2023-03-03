28d19dee0382ec1ccbbb4f8c652d77f6985547e3
Arvind Rajaraman <arvind6902@gmail.com>
2023-03-03 09:01:03 -0800

    Transformer estimator works

--- models.py
+++ models.py
@@ -1,5 +1,8 @@
+import torch
 import torch.nn as nn
 import torch.nn.functional as F
+
+from device import device
 
 class ThetaEstimatorMLP(nn.Module):
     def __init__(self):
@@ -30,3 +33,29 @@ class ThetaEstimatorLargeResidualMLP(nn.Module):
         output = F.relu(self.fc3(output)) + output
         output = F.gelu(self.fc4(output)) + 0.001
         return output
+
+class ThetaEstimatorTransformer(nn.Module):
+    def __init__(self):
+        super().__init__()
+        d_model = 3
+        self.transformer_encoder = nn.TransformerEncoderLayer(d_model=d_model, nhead=3, batch_first=True)
+        self.fc_layers = nn.Sequential(
+            nn.Linear(d_model, 3),
+            nn.ReLU(),
+            nn.Linear(3, 3),
+            nn.ReLU(),
+            nn.Linear(3, 1),
+            nn.Sigmoid()
+        )
+    
+    def forward(self, x):
+        _, layers, _ = x.size()
+        mask = torch.zeros(layers, layers, dtype=torch.bool)
+        for i in range(layers):
+            for j in range(i+1, layers):
+                mask[i, j] = True
+        mask = mask.to(device)
+
+        y = self.transformer_encoder(x, src_mask=mask)
+        y = self.fc_layers(y) + 0.001
+        return y
